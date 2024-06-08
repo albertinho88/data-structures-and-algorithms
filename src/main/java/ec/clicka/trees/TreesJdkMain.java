d7d28bea19b4261298bb0a208174f785ff5caa02
Jlarragamena <jlarragamena@trueblue.com>
2024-06-07 19:19:15 -0500

    Trees and the JDK

---
+++ src/main/java/ec/clicka/trees/TreesJdkMain.java
@@ -0,0 +1,12 @@
+package ec.clicka.trees;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class TreesJdkMain {
+    public static void main(String[] args) {
+        // Most commonly used tree data structures.
+        TreeMap<Integer, String> tree = new TreeMap<>();
+        TreeSet<Integer> treeSet = new TreeSet<>();
+    }
+}
